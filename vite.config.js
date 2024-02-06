c97efec997e4d284e1327624bf011013e8e8abf3
adham <adham.elsharkawy@blueholding.co.uk>
2024-02-06 16:37:38 +0200

    test 2

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio/' // Replace <REPO-NAME> with your GitHub repository name
-    : '/',
+
+	base: '/portfolio/',
 	plugins: [vue()]
 });
